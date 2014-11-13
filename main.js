43dd2d2785f1f2dd524375603cdca6bb838e1944
joseph <thele116@morris.umn.edu>
2014-11-12 20:43:27 -0600

    Did many horrible things trying to get angular routing working to make the menu bar easier to edit. It's really broken now, maybe I'll fix it later.

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-angular.module('mainPage', [])
+angular.module('mainPage', [], function($locationProvider){
+    $locationProvider.html5Mode(true);
+})
+
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider.when("/index.html")   
+    }])
     .controller('mainController', ['$scope', function(){
 
 
     }])
 
-    .controller('navbarCtrl', ['$scope', function($scope, $location){
+    .controller('navbarCtrl', ['$scope', '$location', function($scope, $location){
+
         $scope.menu = [
-            {title:'home', link:'/index.html'},
-            {title:'About', link:'/About.html'}
+            {title:'home', link:'index.html'},
+            {title:'About', link:'About.html'}
         ];
 
         $scope.returnLocation = function(){
+            console.log($location.path());
+            var path = $location.path().split("/");
+            var thing = path[path.length - 1];
+            console.log(thing);
             return $location.path();
         };
 
         $scope.isActive = function(route) {
-            return route === $location.path();
+            var path = $location.path().split("/");
+            return route === path[path.length - 1];
         }
     }]);
