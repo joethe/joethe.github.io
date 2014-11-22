fe8c434718cac8ab8360a02a3304ea0ad6a1a5fd
joseph <thele116@morris.umn.edu>
2014-11-21 19:58:36 -0600

    Did some stuff, started working on projects pages.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-angular.module('mainPage', [])
+angular.module('mainApp', [])
     .controller('mainController', ['$scope', function(){
 
     }])
@@ -6,6 +6,26 @@ angular.module('mainPage', [])
     .controller('navbarCtrl', ['$scope', function($scope){
         $scope.menu = [
             {title:'Home', link:'index.html'},
-            {title:'Projects', link:'Projects.html'}
+            {title:'Projects', link:'Projects.html'},
+            {title:'About', link:'About.html'}
         ];
+    }])
+
+    .controller('projectsCtrl', ['$scope', function($scope){
+        $scope.projectsIndex = [
+            {title:'Playing with virtualization', link:'virtualizationPlayground.html'},
+            {title:'MICS robotics', link:'mics2014.html'}
+        ];
+
+        $scope.activeProject = "virtualizationPlayground.html";
+
+        $scope.setActiveProject = function(link){
+            $scope.activeProject = link;
+        };
+
+        $scope.isActiveProject = function(link){
+            return $scope.activeProject === link;
+        }
+
+
     }]);
