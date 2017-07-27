96faa095aabf51ad6dc52625430525b860c6dbfc
Joseph <thele116@morris.umn.edu>
2017-07-27 17:39:37 -0500

    Replace Angular in Navbar with JQuery.

---
+++ components/navbar/navbar.js
@@ -0,0 +1,14 @@
+var navItems = [
+  {title: 'Home', link:'index.html'},
+  {title: 'Projects', link:'projects.html'},
+  {title: 'Contact', link:'contact.html'}
+];
+
+var navHtml = "";
+
+$.each(navItems, function(index, value){
+  var newItem = "<li><a href=\""+ value.link + "\">" + value.title + "</a></li>";
+  navHtml += newItem;
+});
+
+$("list#navList").html(navHtml);
