bb2385da87623b8efef814ed78c84d15815b04c9
Joseph <thele116@morris.umn.edu>
2017-07-28 17:38:36 -0500

    Add project page for MICS 2016
    Fixes #4

--- projects.js
+++ projects.js
@@ -2,6 +2,7 @@ var projects = [
   {title: 'Playing with virtualization', link:'virtualizationPlayground.html'},
   {title: 'MICS Robotics (2014)', link:'mics2014.html'},
   {title: 'MICS Robotics (2015)', link:'mics2015.html'},
+  {title: 'MICS Robotics (2016)', link:'mics2016.html'},
   {title: 'URSA Major', link: 'ursaMajor.html'},
   {title: 'Software Dev. Support', link: 'softwareDevSetup.html'}
 ];
